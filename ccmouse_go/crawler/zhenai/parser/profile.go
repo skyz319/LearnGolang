7c26d0caf64840997d89ece975ea443e31a568e2
Skyz319 <cain.code@qq.com>
2019-04-13 17:41:51 +0800

    regexp

--- ccmouse_go/crawler/zhenai/parser/profile.go
+++ ccmouse_go/crawler/zhenai/parser/profile.go
@@ -1,7 +1,47 @@
 package parser
 
-const ageRe = ``
+import (
+	"LearnGolang/ccmouse_go/crawler/engine"
+	"regexp"
+)
 
-//func ParseProfile(contents []byte) engine.ParseResult {
-//
-//}
+//	昵称
+var nickRe = regexp.MustCompile(`<h1 class="nickName" .*?>([^<]+)</h1>`)
+
+//	个人信息1
+var infoRe1 = regexp.MustCompile(`<div class="m-btn .*?>([^<]+)</div>`)
+
+//	个人信息2
+var infoR22 = regexp.MustCompile(`<div class="m-btn pink" .*?>([^<]+)</div>`)
+
+//	择偶条件
+var demandRe = regexp.MustCompile(`<div class="m-btn" .*?>([^<]+)</div>`)
+
+//	相片
+var photoRe = regexp.MustCompile(`<div .*? href="([^"]+)" class="photoItem z-cursor-big.*?">`)
+
+//	<div class="purple-btns" data-v-bff6f798=""><div class="m-btn purple" data-v-bff6f798="">未婚</div><div class="m-btn purple" data-v-bff6f798="">43岁</div><div class="m-btn purple" data-v-bff6f798="">魔羯座(12.22-01.19)</div><div class="m-btn purple" data-v-bff6f798="">166cm</div><div class="m-btn purple" data-v-bff6f798="">56kg</div><div class="m-btn purple" data-v-bff6f798="">工作地:鞍山铁东区</div><div class="m-btn purple" data-v-bff6f798="">月收入:3千以下</div><div class="m-btn purple" data-v-bff6f798="">美容师</div><div class="m-btn purple" data-v-bff6f798="">中专</div></div>
+
+func ParseProfile(contents []byte) engine.ParseResult {
+
+	//re := regexp.MustCompile(ageRe)
+	//match := re.FindAllSubmatch(contents, -1)
+	//
+	//if match != nil {
+	//}
+
+	return engine.ParseResult{}
+}
+
+func extractString(contents []byte, re *regexp.Regexp) string {
+
+	match := re.FindSubmatch(contents)
+
+	if len(match) >= 2 {
+
+		return string(match[1])
+	} else {
+
+		return ""
+	}
+}
--- ccmouse_go/regex/main.go
+++ ccmouse_go/regex/main.go
@@ -13,6 +13,9 @@ skyz_319@tom.com
 skyz_319@tom.com.cn
 `
 
+const text2 = `
+<div class="logo f-fl" style="background-image:url(https://photo.zastatic.com/images/photo/308779/1235115550/4612939340901982.jpg?scrop=1&amp;crop=1&amp;cpos=north&amp;w=200&amp;h=200);" data-v-5b109fc3=""></div>`
+
 func main() {
 
 	//	匹配字串
@@ -22,13 +25,18 @@ func main() {
 	//fmt.Println(match)
 
 	//	正则匹配
-	re := regexp.MustCompile(`[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.]+`)
-	match := re.FindAllString(text, -1)
-	fmt.Println(match)
+	//re := regexp.MustCompile(`[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.]+`)
+	//match := re.FindAllString(text, -1)
+	//fmt.Println(match)
 
-	//	正则提取
-	re2 := regexp.MustCompile(`([a-zA-Z0-9._]+)@([a-zA-Z0-9]+)(\.[a-zA-Z0-9.]+)`)
-	match2 := re2.FindAllStringSubmatch(text, -1)
-	fmt.Println("match2:", match2)
+	////	正则提取
+	//re2 := regexp.MustCompile(`([a-zA-Z0-9._]+)@([a-zA-Z0-9]+)(\.[a-zA-Z0-9.]+)`)
+	//match2 := re2.FindAllStringSubmatch(text, -1)
+	//fmt.Println("match2:", match2)
+
+	reg3 := regexp.MustCompile(`<div <div class="logo f-fl" style=".*? (^(\w+):\/\/([^/:]+)\/([^/:]+)\/(\d*)?\/(\d*)$[^?]+).*?" .*?></div>`)
+
+	match3 := reg3.FindAllStringSubmatch(text2, -1)
+	fmt.Println("match3:", match3)
 
 }
